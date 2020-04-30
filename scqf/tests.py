5ae4c49d94817f6526a2ecc7272f6ab097829ac5
Nikola Novarlic <35609096+greendate@users.noreply.github.com>
2020-04-30 17:45:11 +0300

    tests are written

--- scqf/tests.py
+++ scqf/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from .models import Club, Feedback
 
 # Create your tests here.
+
+class GradeTestCase1(TestCase):
+    def setUp(self):
+        Club.objects.create(club_id = 1, grade=4.5)
+        Feedback.objects.create(club_id = 1, grade = 4)
+        Feedback.objects.create(club_id = 1, grade = 5)
+
+    def test_correct_grade(self):
+        clubs = Club.objects.all()
+        for club in clubs:
+            feedbacks = Feedback.objects.filter(club_id=club.club_id)
+            sum = 0
+            for feedback in feedbacks:
+                sum += feedback.grade
+            count = Feedback.objects.filter(club_id=club.club_id).count()
+            self.assertEqual(float(club.grade), round(sum / count, 1))
+
+
+class GradeTestCase2(TestCase):
+    def setUp(self):
+        Club.objects.create(club_id = 1, grade=2.8)
+        Club.objects.create(club_id = 2, grade=2.5)
+        Feedback.objects.create(club_id = 1, grade = 4)
+        Feedback.objects.create(club_id = 2, grade = 3)
+        Feedback.objects.create(club_id = 1, grade = 1)
+        Feedback.objects.create(club_id = 2, grade = 2)
+        Feedback.objects.create(club_id = 1, grade = 2)
+        Feedback.objects.create(club_id = 1, grade = 4)
+
+    def test_correct_grade(self):
+        clubs = Club.objects.all()
+        for club in clubs:
+            feedbacks = Feedback.objects.filter(club_id=club.club_id)
+            sum = 0
+            for feedback in feedbacks:
+                sum += feedback.grade
+            count = Feedback.objects.filter(club_id=club.club_id).count()
+            self.assertEqual(float(club.grade), round(sum / count, 1))
